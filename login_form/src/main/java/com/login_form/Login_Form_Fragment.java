5cf785d1743ff3063c48e010c712bf0499f6db16
VIRAK33 <seamvirak.itc@gmail.com>
2019-09-19 01:08:50 +0700

    Initial commit

---
+++ login_form/src/main/java/com/login_form/Login_Form_Fragment.java
@@ -0,0 +1,19 @@
+package com.login_form;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Login_Form_Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.login,container);
+        return view;
+    }
+}
---
+++ login_form/src/main/java/com/login_form/MainActivity_Login.java
@@ -0,0 +1,16 @@
+package com.login_form;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
